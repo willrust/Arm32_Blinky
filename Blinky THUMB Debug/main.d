377f3186344a632fc443aa6ea571a24c0dd39964
William Rust <willrust@gmail.com>
2014-04-03 20:07:41 -0700

    Adds STM32L1xx... path to project.

--- Blinky THUMB Debug/main.d
+++ Blinky THUMB Debug/main.d
@@ -2,4 +2,11 @@ Blinky\ THUMB\ Debug/main.o: \
  /Users/williamrust/Workspace/Tests/Arm32_Blinky/main.c \
  /Applications/CrossWorks\ for\ ARM\ 3.0/include/__cross_studio_io.h \
  /Applications/CrossWorks\ for\ ARM\ 3.0/include/cross_studio_io.h \
- /Applications/CrossWorks\ for\ ARM\ 3.0/include/__crossworks.h
+ /Applications/CrossWorks\ for\ ARM\ 3.0/include/__crossworks.h \
+ Vendor/STM32L1xx_StdPeriph_Lib/Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h \
+ /Users/williamrust/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/STM32/include/stm32l1xx.h \
+ /Users/williamrust/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cm3.h \
+ /Applications/CrossWorks\ for\ ARM\ 3.0/include/stdint.h \
+ /Users/williamrust/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cmInstr.h \
+ /Users/williamrust/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/CMSIS_3/CMSIS/Include/core_cmFunc.h \
+ /Users/williamrust/Library/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v3/packages/targets/STM32/include/system_stm32l1xx.h
